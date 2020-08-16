26afb9f752b7432f50a9f1babf419486d6464978
bassix <adam@net-i.de>
2020-08-16 15:47:56 +0200

    PHP code style fixed

--- app/src/Service/VoucherService.php
+++ app/src/Service/VoucherService.php
@@ -8,10 +8,9 @@ use App\Entity\VoucherEntity;
 
 class VoucherService
 {
-    private const MINIMUM_AMOUNT = '100';
     public const CODE_LENGTH = 8;
 
-    protected function generateCode() { return substr(md5((string)mt_rand()), 0, self::CODE_LENGTH); }
+    private const MINIMUM_AMOUNT = '100';
 
     public function apply(OrderEntity $order): ?VoucherEntity
     {
@@ -20,5 +19,10 @@ class VoucherService
         }
 
         return (new VoucherEntity())->setOrder($order)->setCode($this->generateCode());
+    }
+
+    protected function generateCode()
+    {
+        return substr(md5((string)mt_rand()), 0, self::CODE_LENGTH);
     }
 }
