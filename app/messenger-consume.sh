d49b9a120b2a0b0693849c2201602fe046e9efab
bassix <adam@net-i.de>
2020-08-19 16:57:30 +0200

    The interval of restart reduced in case of the app crashes customer don't need to wait so long ;)

--- app/messenger-consume.sh
+++ app/messenger-consume.sh
@@ -4,6 +4,6 @@ sleep 5
 
 while true;
 do
-    bin/console messenger:consume --time-limit=295 --memory-limit=128M >&1
-    sleep 295
+    bin/console messenger:consume --time-limit=55 --memory-limit=128M >&1
+    sleep 56
 done
